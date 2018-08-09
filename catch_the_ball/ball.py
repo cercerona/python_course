a9d99bde835661c438ff4a7ef0152709813cf757
Dmitry Kononov <dmitrij.kononov@gmail.com>
2018-08-09 11:23:54 +0300

    Добавлены документ-строки, заготовка для функции генерации цвета

--- catch_the_ball/ball.py
+++ catch_the_ball/ball.py
@@ -7,11 +7,26 @@ def click_ball(event):
     """
     print(event.x, event.y)
 
+def random_color():
+    """Возвращает случайное значение цвета
+    :param: нет
+    :return: цвет
+    """
+    pass #fixme
+
+
 def create_random_ball():
+    """Создает круг, закрашенный случайным цветом
+    :param ничего
+    :return: ничего
+    """
     canvas.create_oval(x, y, x+2*R, y+2*R, width=1, fill=random_color())
 
 def init_main_window():
-    """Создает и инициирует виджеты окна игры"""
+    """Создает и инициирует виджеты окна игры
+    :param: нет
+    :return: ничего
+    """
     global root, canvas #создание глобальных виджетов главного окна и холста
     root = tkinter.Tk()
     canvas = tkinter.Canvas(root, background='white', width=400, height=400)
