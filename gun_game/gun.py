fa147e0c0d464b43cbfdfb698b5b04dc4bb4cfc6
Dmitry Kononov <dmitrij.kononov@gmail.com>
2018-08-17 12:16:02 +0300

    Добавлено движение шариков с отбивкой от стены. Реализован таймер вызова нового метода fly() класса Ball, отвечающего за движение шариков.

--- gun_game/gun.py
+++ gun_game/gun.py
@@ -3,16 +3,38 @@ from random import choice, randint
 
 canvas_width = 400
 canvas_height = 300
+timer_delay = 120# определяет задержку в вызове таймера
 
 class Ball:
-    initial_number = 20#число шариков для игры
+    initial_number = 20# число шариков для игры
     minimal_radius = 15
     maximal_radius = 40
     available_colors = ["green", "blue", "red"]
-    minimal_Vx = -2#Минимальное значение скорости по оси X
-    maximal_Vx = 2#Максимальное значение скорости по оси X
-    minimal_Vy = -2#Минимальное значение скорости по оси Y
-    maximal_Vy = 2 #Максимальное значение скорости по оси Y
+    minimal_Vx = -2# Минимальное значение скорости по оси X
+    maximal_Vx = 2# Максимальное значение скорости по оси X
+    minimal_Vy = -2# Минимальное значение скорости по оси Y
+    maximal_Vy = 2 # Максимальное значение скорости по оси Y
+
+    def fly(self):
+        """Отбиваемся от стены"""
+        self._x = self._x + self._Vx
+        self._y = self._y + self._Vy
+
+        if self._x <= 0:
+            self._x = 0
+            self._Vx = -1*self._Vx
+        elif self._x + 2*self._R >= canvas_width:
+            self._x = canvas_width-2*self._R-1
+            self._Vx = -1*self._Vx
+        if self._y <= 0:
+            self._y = 0
+            self._Vy = -1*self._Vy
+        elif self._y + 2*self._R >= canvas_height:
+            self._y = canvas_height-2*self._R-1
+            self._Vy = -1*self._Vy
+
+        canvas.coords(self._avatar, self._x, self._y, self._x + 2*self._R, self._y + 2*self._R)
+
 
     def __init__(self):
         """
@@ -21,7 +43,7 @@ class Ball:
         """
         R = randint(Ball.minimal_radius, Ball.maximal_radius)
         x = randint(0+R, canvas_width-1-2*R)
-        y = randint(0+R, canvas_width-1-2*R)
+        y = randint(0+R, canvas_height-1-2*R)
         fill_color = choice(Ball.available_colors)
         self._R = R
         self._x = x
@@ -51,11 +73,19 @@ def init_main_window():
     scores_value = tkinter.IntVar()
     canvas = tkinter.Canvas(root, background='white', width=canvas_width, height=canvas_height)
     scores_text = tkinter.Entry(root, textvariable = scores_value)
-    canvas.pack()
-    scores_text.pack()
+    canvas.grid(row = 1, column = 0, columnspan = 3)
+    scores_text.grid(row = 0, column = 2)
+
+def timer_event():
+    """Запускает таймер каждые timer_delay миллисекунд, определенных в глобальной переменной timer_delay.
+    Двигает шарик, вызывая метод fly()"""
+    for ball in balls:
+        ball.fly()
+    canvas.after(timer_delay, timer_event)
 
 
 if __name__ == "__main__":
     init_main_window()
     init_game()
+    timer_event()
     root.mainloop()
