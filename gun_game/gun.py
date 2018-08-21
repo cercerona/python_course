d4be6601c1f999d8cd1eb4e871b6aa63e0c3bb37
Dmitry Kononov <dmitrij.kononov@gmail.com>
2018-08-21 14:06:25 +0300

    Добавляем физику! Реализовали физику движения. Изменили скорость пули и квант времени, для более выразительного движения пули.Реализация работоспособна.

--- gun_game/gun.py
+++ gun_game/gun.py
@@ -3,7 +3,9 @@ from random import choice, randint
 
 canvas_width = 400
 canvas_height = 300
-timer_delay = 120# определяет задержку в вызове таймера
+timer_delay = 120 # определяет задержку в вызове таймера
+gravitational_acceleration = 9.8 # Ускорение свободного падения
+dt = 0.1 # квант физического времени
 
 class MovingUnit:
     """Абстрактный класс, определяющий движущийся объект (шарик-мишень, шарик-пулю)
@@ -11,7 +13,7 @@ class MovingUnit:
     и абстрактный (запрещенный к ввызову извне) метод fly()
     Наследниками являются классы шарика-мишени (Ball) и шарика-пули (Shell)"""
 
-    def __init__(self, x, y, Vx, Vy, avatar):
+    def __init__(self, x, y, Vx, Vy, R, avatar):
         self._x  = x
         self._y = y
         self._Vx = Vx
@@ -23,7 +25,44 @@ class MovingUnit:
         """Абстрактный метод fly()"""
         raise RuntimeError
 
-class Ball:# Класс, определяющий объект-шарик
+
+class Shell(MovingUnit):
+    """
+    Класс, определяющий объекты-пули
+    Не отражается от стенок, уничтожается, если вылетает за пределы поля.
+    Двигается по гравитационной траектории.
+    """
+    radius = 5
+    maximal_number = 3
+    color = 'black'
+    def __init__(self, x, y, Vx, Vy):
+        """
+
+        """
+        R = Shell.radius
+        fill_color = Shell.color
+        avatar = canvas.create_oval(x, y, x+2*R, y+2*R, width=1, fill=fill_color, outline=fill_color)
+        super().__init__(x, y, Vx, Vy, R, avatar)
+
+    def fly(self):
+        """
+        Определяет гравитационную траекторию движения пули
+        """
+        ax = 0 # Проекция ускорения на ось X
+        ay = gravitational_acceleration # Проекция ускорения на ось Y
+        self._x += self._Vx*dt + ax*dt**2/2
+        self._y += self._Vy*dt + ay*dt**2/2
+        self._Vx += ax*dt
+        self._Vy += ay*dt
+
+        canvas.coords(self._avatar, self._x, self._y, self._x + 2*self._R, self._y + 2*self._R) # смещение координат
+
+        #fixme: пока не отслеживается вылет за пределы поля, когда снаряд надо униичтожить
+
+class Ball(MovingUnit):
+    """
+    Класс, определяющий шарики-мишени
+    """
     initial_number = 20# число шариков для игры
     minimal_radius = 15
     maximal_radius = 40
@@ -32,6 +71,22 @@ class Ball:# Класс, определяющий объект-шарик
     maximal_Vx = 2# Максимальное значение скорости по оси X
     minimal_Vy = -2# Минимальное значение скорости по оси Y
     maximal_Vy = 2 # Максимальное значение скорости по оси Y
+
+    def __init__(self):
+        """
+        Создаем объект-шар в случайной позиции на холсте, закрашенный случайным цветом и
+        со случайными значениями скоростей по осям X и Y
+        """
+        R = randint(Ball.minimal_radius, Ball.maximal_radius)
+        x = randint(0+R, canvas_width-1-2*R)
+        y = randint(0+R, canvas_height-1-2*R)
+        Vx = randint(Ball.minimal_Vx, Ball.maximal_Vx)
+        Vy = randint(Ball.minimal_Vy, Ball.maximal_Vy)
+
+        fill_color = choice(Ball.available_colors)
+        avatar = canvas.create_oval(x, y, x+2*R, y+2*R, width=1, fill=fill_color,  outline=fill_color)
+
+        super().__init__(x, y, Vx, Vy, R, avatar)
 
     def fly(self):
         """Отбиваемся от стены"""
@@ -51,23 +106,8 @@ class Ball:# Класс, определяющий объект-шарик
             self._y = canvas_height-2*self._R-1
             self._Vy = -1*self._Vy
 
-        canvas.coords(self._avatar, self._x, self._y, self._x + 2*self._R, self._y + 2*self._R)
+        canvas.coords(self._avatar, self._x, self._y, self._x + 2*self._R, self._y + 2*self._R) # смещение координат
 
-    def __init__(self):
-        """
-        Создаем объект-шар в случайной позиции на холсте, закрашенный случайным цветом и
-        со случайными значениями скоростей по осям X и Y
-        """
-        R = randint(Ball.minimal_radius, Ball.maximal_radius)
-        x = randint(0+R, canvas_width-1-2*R)
-        y = randint(0+R, canvas_height-1-2*R)
-        fill_color = choice(Ball.available_colors)
-        self._R = R
-        self._x = x
-        self._y = y
-        self._avatar = canvas.create_oval(x, y, x+2*R, y+2*R, width=1, fill=fill_color)
-        self._Vx = randint(Ball.minimal_Vx, Ball.maximal_Vx)
-        self._Vy = randint(Ball.minimal_Vy, Ball.maximal_Vy)
 
 class Gun:# Класс, определяющий объект-пушку
     def __init__(self):
@@ -82,13 +122,7 @@ class Gun:# Класс, определяющий объект-пушку
         Метод, осуществляющий выстрел.
         :return Возвращает объект-пулю
         """
-        shell = Ball()# Создаем объект-пулю
-        shell._x = self._x + self._lx
-        shell._y = self._y + self._ly
-        shell._Vx = self._lx/10
-        shell._Vy = self._ly/10
-        shell._R = 5
-        shell.fly()
+        shell = Shell(self._x + self._lx, self._y + self._ly, 2*self._lx, 2*self._ly)# Создаем объект-пулю
         return shell
 
 
