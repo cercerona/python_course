22942da353df8a8f7e762d2ec6d134af95dd6b6e
Dmitry Kononov <dmitrij.kononov@gmail.com>
2018-11-01 17:28:35 +0300

    Учебная СУБД на примерах из книги М.Лутц "Программируем на Python". Модуль инициализации СУБД.

---
+++ db/initdata.py
@@ -0,0 +1,15 @@
+#  Инициализировать данные для последующего сохранения в файлах
+#  Записи
+petr = {'name': 'Petr Petrov', 'age': 14, 'class': '8', 'letter': 'a', 'interest': 'chess'}
+elen = {'name': 'Elen Sidorova', 'age': 14, 'class': '8', 'letter': 'a', 'interest': 'cooking'}
+ivan = {'name': 'Ivan Ivanov', 'age': 13, 'class': '8', 'letter': 'b', 'interest': 'football'}
+
+#  база данных
+db = {}
+db['petr'] = petr
+db['elen'] = elen
+db['ivan'] = ivan
+
+if __name__ == "__main__":
+    for key in db:
+        print(key, '=>\n', db[key])
