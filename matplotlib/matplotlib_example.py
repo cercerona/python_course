9acdbff74c00efcacd6b59e70de179e41d9bc547
Dmitry Kononov <cercerona@gmail.com>
2018-11-21 12:09:21 +0300

    Отработка построения графиков в matplotlib. Подключаем библиотеки.

---
+++ matplotlib/matplotlib_example.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# matplotlib plotting module
+import matplotlib.pyplot as plt
+
+# matplotlib colormap module
+import matplotlib.cm as cm
+
+# needed for formatting Y axis
+from matplotlib.ticker import FuncFormatter
+
+# Matplotlib font manager
+import matplotlib.font_manager as font_manager
