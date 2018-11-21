8c33b7584f8f74d2b461b04a47eef83fcecc991d
Dmitry Kononov <cercerona@gmail.com>
2018-11-21 12:16:00 +0300

    Отработка построения графиков в matplotlib. Добавил функцию чтения из csv файла.

--- matplotlib/matplotlib_example.py
+++ matplotlib/matplotlib_example.py
@@ -1,4 +1,6 @@
 import numpy as np
+import csv
+import pprint
 
 # matplotlib plotting module
 import matplotlib.pyplot as plt
@@ -11,3 +13,30 @@ from matplotlib.ticker import FuncFormatter
 
 # Matplotlib font manager
 import matplotlib.font_manager as font_manager
+
+"""Include functions for read csv files with WAB report"""
+def read_report(file_obj):
+    """
+    Function for read report from csv file
+    :param file_obj: file to read
+    :return: table - report in table with rows of the file
+    """
+    return [row for row in csv.reader(file_obj)]
+
+# bar width
+width = .8
+
+if __name__ == '__main__':
+    file_name = [r'C:\Users\Dmitry Kononov\Documents\Бастион\Python\Отчеты\Примеры выгрузок\БСпб\statistics\srv-wab-dca-2.bankspb.ru_2018-07-20T17_55_s_connections_per_device.csv',\
+                 r'C:\Users\Dmitry Kononov\Documents\Бастион\Python\Отчеты\Примеры выгрузок\БСпб\statistics\srv-wab-dca-2.bankspb.ru_2018-07-20T17_57_s_connections_per_account.csv',\
+                 r'C:\Users\Dmitry Kononov\Documents\Бастион\Python\Отчеты\Примеры выгрузок\БСпб\statistics\srv-wab-dca-2.bankspb.ru_2018-07-20T18_02_s_connections_per_user.csv',\
+                 r'C:\Users\Dmitry Kononov\Documents\Бастион\Python\Отчеты\Примеры выгрузок\БСпб\statistics\srv-wab-dca-2.bankspb.ru_2018-07-20T18_03_s_connections_duration_per_user.csv',\
+                 r'C:\Users\Dmitry Kononov\Documents\Бастион\Python\Отчеты\Примеры выгрузок\БСпб\statistics\srv-wab-dca-2.bankspb.ru_2018-07-20T18_03_s_connections_per_duration.csv',\
+                 r'C:\Users\Dmitry Kononov\Documents\Бастион\Python\Отчеты\Примеры выгрузок\БСпб\statistics\srv-wab-dca-2.bankspb.ru_2018-07-20T18_04_s_connections_per_date.csv',\
+                 r'C:\Users\Dmitry Kononov\Documents\Бастион\Python\Отчеты\Примеры выгрузок\БСпб\statistics\srv-wab-dca-2.bankspb.ru_2018-07-20T18_04_s_max_simultaneous_connections.csv']
+    table = []
+    with open(file_name[6], 'r') as infile:
+        table = read_report(infile)
+
+    pprint.pprint(table)
+    print('Всего записей: ', len(table))
